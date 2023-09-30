52b3ed4aadb5140230a3cef0c15e5d923c9047b8
Gary Guo <gary@garyguo.net>
2023-09-30 21:36:56 +0800

    rust: bindings: rename const binding using sed
    Current for consts that bindgen don't recognise, we define a helper
    constant with
    
        const <TYPE> BINDINGS_<NAME> = <NAME>;
    
    in `bindings_helper.h` and then we put
    
        pub const <NAME>: <TYPE> = BINDINGS_<NAME>;
    
    in `bindings/lib.rs`. This is fine that we currently only have 3
    constants that are defined this way, but is going to be more annoying
    when more constants are added since every new constant needs to be
    defined in two places.
    
    This patch changes the way we define constant helpers to
    
        const <TYPE> RUST_BINDING_<NAME> = <NAME>;
    
    and then use `sed` to postprocess Rust code by generated by bindgen to
    remove the distinct prefix, so user of the binding crate can refer to
    the name directly.
    
    Signed-off-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Martin Rodriguez Reboredo <yakoyoku@gmail.com>
    Link: https://lore.kernel.org/r/20230930133704.13313-1-gary@garyguo.net
    [boqun: Resolve conflicts with "grep -Ev" changes]

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -15,6 +15,6 @@
 #include <linux/workqueue.h>
 
 /* `bindgen` gets confused at certain things. */
-const size_t BINDINGS_ARCH_SLAB_MINALIGN = ARCH_SLAB_MINALIGN;
-const gfp_t BINDINGS_GFP_KERNEL = GFP_KERNEL;
-const gfp_t BINDINGS___GFP_ZERO = __GFP_ZERO;
+const size_t RUST_BINDING_ARCH_SLAB_MINALIGN = ARCH_SLAB_MINALIGN;
+const gfp_t RUST_BINDING_GFP_KERNEL = GFP_KERNEL;
+const gfp_t RUST_BINDING___GFP_ZERO = __GFP_ZERO;
